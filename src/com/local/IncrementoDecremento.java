e7d6a76091ba80e2b4075b6803d1cd2b7e456697
Walter Donizety Cargnieli <cargnieli@yahoo.com>
2021-04-30 21:27:32 -0300

    Incremento/Decremento.

---
+++ src/com/local/IncrementoDecremento.java
@@ -0,0 +1,17 @@
+package com.local;
+
+public class IncrementoDecremento {
+
+    public static void main(String[] args) {
+
+        int idade = 10;
+        int idade1 = 10;
+        int novaIdade = --idade1;
+        int novaIdade1 = ++idade;
+
+
+        System.out.println("Nova idade: " + novaIdade);
+        System.out.println("Nova Idade1: "+ novaIdade1);
+
+    }
+}
